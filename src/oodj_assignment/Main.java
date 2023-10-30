3cb51685e4013166d1418181f319dbf26b0c449e
gec <ecgoh0809@gmail.com>
2023-10-30 11:24:34 +0800

    fuck

--- src/oodj_assignment/Main.java
+++ src/oodj_assignment/Main.java
@@ -14,7 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World 111");
+        System.out.println("Hello World 11");
     }
     
 }
