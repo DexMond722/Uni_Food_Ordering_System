9f39bec2d6077d7a4dbf155b297fc4165896d45f
Desmond Chen <desmondchen2003@gmail.com>
2023-12-01 21:56:44 +0800

    added feature - generate transaction log

--- src/Class/UserCredit.java
+++ src/Class/UserCredit.java
@@ -4,7 +4,13 @@
  */
 package Class;
 
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
@@ -25,12 +31,63 @@ public class UserCredit extends UserManager {
             double updatedCredit = currentCredit + topUpAmount;
             user.setCredit(updatedCredit);
 
+            generateTransactionRecord(user.getId(), topUpAmount);
+
             userManager.saveUsers();
 
             return true;
         }
         return false;
     }
+
+    private void generateTransactionRecord(int userId, double transactionAmount) {
+        String transactionID = generateTransactionID();
+        String dateTime = getCurrentDateTime();
+        String transactionType = "Debit";
+        String remark = "CreditTopUp";
+
+        String transactionRecord = String.format("%s,%d,%.2f,%s,%s,%s",
+                transactionID, userId, transactionAmount, dateTime, transactionType, remark);
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("src/Database/credit_transaction.txt", true))) {
+            bw.write(transactionRecord);
+            bw.newLine();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String generateTransactionID() {
+        int nextTransactionID = getNextTransactionID();
+        return String.format("T%05d", nextTransactionID);
+    }
+
+    private int getNextTransactionID() {
+        int lastTransactionID = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader("src/Database/credit_transaction.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                if (data.length > 0) {
+                    try {
+                        String numericPart = data[0].substring(1);
+                        int transactionID = Integer.parseInt(numericPart);
+                        lastTransactionID = Math.max(lastTransactionID, transactionID);
+                    } catch (NumberFormatException | StringIndexOutOfBoundsException e) {
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return lastTransactionID + 1;
+    }
+
+    private String getCurrentDateTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return dateFormat.format(new Date());
+    }
 }
-
-
